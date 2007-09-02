e81979d5e4b293cdee3f4e7943b359b142779649
Wayne Larsen <wlarsen@Buduo.local>
2007-09-01 20:33:25 -0400

    updates to structure

--- roar/spec/spec_helper.rb
+++ roar/spec/spec_helper.rb
@@ -1,9 +1,12 @@
+ENV["RAILS_ENV"] = "test"
+RAILS_ROOT = File.dirname(__FILE__)+'/../../roar_test'
+require "#{RAILS_ROOT}/config/environment"
+
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
-$LOAD_PATH.unshift File.dirname(__FILE__) + '/../../criteriaquery/lib'
-require 'rubygems'
-require 'active_support'
-require 'breakpoint'
-require 'query' 
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../../vendor/criteriaquery/lib'
+# require 'rubygems'
+#require 'activesupport'
+# require 'query' 
 %w(actions action column form field filter collection base).each { |klass| require "roar/#{klass}" }
 require 'roar/rails/act_methods'
 require 'roar/rails/base_controller'
